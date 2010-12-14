01fd3c5cc8a66e416b6a4d6af66fd96c7bf492c4
Alan Malloy <alan@malloys.org>
2010-12-13 17:13:05 -0800

    Add longopt support

--- src/cljopts/getopt.clj
+++ src/cljopts/getopt.clj
@@ -7,33 +7,34 @@
            (.getopt gnu-obj))
    (.getOptarg gnu-obj)])
 
+(defn make-getopt
+  ([prog-name opt-string argv]
+     (make-getopt prog-name opt-string (make-array LongOpt 0) argv))
+  ([prog-name opt-string long-opts argv]
+     (Getopt. prog-name (into-array String (seq argv)) opt-string long-opts)))
+
 (defn getopt-seq
   "Turn iterative calls to gnu getopt into a lazy seq of return valuse.
 
 => (getopt-seq nil \"ab:c::d\" (into-array String [\"-ab\" \"10\"]))
      ([\\a nil] [\\b \"10\"])"
-  [prog-name opt-string argv]
-  (let [getopt (Getopt. prog-name argv opt-string)]
-    (map (fn [[opt arg]]
-           [(-> opt char str keyword)
-            arg])
-         (take-while first
-                     (repeatedly #(read-opt getopt))))))
+  [getopt]
+  (map (fn [[opt arg]]
+            [(-> opt char str keyword)
+             arg])
+          (take-while first
+                      (repeatedly #(read-opt getopt)))))
 
 (defn getopt-map
-  "Turn an option seq into a map of option=>value pairs. If 
+  "Turn an option seq into a map of keyword=>[values] pairs. If 
 
-=> (getopt-map (getopt-seq \"getopt\" \"ab:c::d\" (into-array String [\"-ab\" \"10\"])))
-{:b \"10\", :a nil}
-=> (getopt-map (getopt-seq \"getopt\" \"ab:c::d\" (into-array String [\"-ab\" \"10\" \"-b\" \"1\"])))
-{:b [\"10\" \"1\"], :a nil}"
+=> (getopt-map (getopt-seq nil \"ab:c::d\" (into-array String [\"-ab\" \"10\"])))
+{:b [\"10\"], :a [nil]}
+=> (getopt-map (getopt-seq nil \"ab:c::d\" (into-array String [\"-ab\" \"10\" \"-b\" \"1\"])))
+{:b [\"10\" \"1\"], :a [nil]}"
   [opt-seq]
   (reduce
    (fn [m [opt arg]]
      (update-in m [opt]
-                (fn [v]
-                  (cond
-                   (nil? v) arg
-                   (vector? v) (conj v arg)
-                   :else [v arg]))))
-   {} opt-seq)) 
+                (fnil conj []) arg))
+   {} opt-seq))
