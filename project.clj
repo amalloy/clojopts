866ad9baa94fe120224d88231127f15a53d9023d
Alan Malloy <alan@malloys.org>
2011-01-03 00:07:21 -0800

    Bump version number - maybe nearing a real release?

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojopts "0.2.0"
+(defproject clojopts "0.3.0"
   :description "Command-line library for Clojure: a wrapper around GNU GetOpt, and some additional higher-level features."
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]
