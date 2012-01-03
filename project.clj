f93aa2fd80566c9f652d94c0457c1816234e3609
Alan Malloy <alan@malloys.org>
2012-01-02 18:18:47 -0800

    New version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojopts "0.3.1"
+(defproject clojopts "0.3.2"
   :description "Command-line library for Clojure: a wrapper around GNU
   GetOpt, and some additional higher-level features."
   :url "https://github.com/amalloy/clojopts"
@@ -7,4 +7,3 @@
                  [gnu.getopt/java-getopt "1.0.13"]]
   ; apache doesn't host getopt for some reason, so use sun's repo
   :repositories {"sun" "http://download.java.net/maven/2"})
-
