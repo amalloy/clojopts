5f3ba63804bc507ea33de716a40abfe9d4a674e5
Alan Malloy <alan@malloys.org>
2014-05-27 14:40:13 -0700

    Add test case for ::more

--- test/clojopts/test/core.clj
+++ test/clojopts/test/core.clj
@@ -4,8 +4,9 @@
 
 (deftest integration
   (is (= (clojopts "clojopts"
-                   ["--name=src/clojure/awesome.clj" "-n" "10"]
+                   ["--name=src/clojure/awesome.clj" "y" "-n" "10" "--" "-foo" "x"]
                    (optional-arg file f name "The file to use"
                                  :parse #(java.io.File. %))
                    (with-arg lines n "How many lines to read"))
-         {:file (java.io.File. "src/clojure/awesome.clj") :lines 10})))
+         {:file (java.io.File. "src/clojure/awesome.clj") :lines 10
+          :clojopts/more ["y" "-foo" "x"]})))
