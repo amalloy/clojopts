b1d2e0fb43aa771b380f96045c29c1879b9fb8e7
Alan Malloy <alan@malloys.org>
2014-05-27 14:40:02 -0700

    Update clojure and lein versions

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Command-line library for Clojure: a wrapper around GNU
   GetOpt, and some additional higher-level features."
   :url "https://github.com/amalloy/clojopts"
-  :dependencies [[clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [useful "0.7.6-alpha1"]
                  [gnu.getopt/java-getopt "1.0.13"]]
   ; apache doesn't host getopt for some reason, so use sun's repo
