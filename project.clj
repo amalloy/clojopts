90e28f91a694fb45be122219fa7ca99e209fc43b
Alan Malloy <alan@malloys.org>
2014-05-27 17:50:19 -0700

    Maven central hosts getopt now

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
   :url "https://github.com/amalloy/clojopts"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [useful "0.7.6-alpha1"]
-                 [gnu.getopt/java-getopt "1.0.13"]]
-  ; apache doesn't host getopt for some reason, so use sun's repo
-  :repositories {"sun" "http://download.java.net/maven/2"})
+                 [gnu.getopt/java-getopt "1.0.13"]])
